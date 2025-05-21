152aaa051ab03107838683cc02285f04b437a1b2
Raman Singh <kumarraman1694@gmail.com>
2025-05-21 15:43:04 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,17 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'staticfiles'
 ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_ROOT = os.path.join(BASE_DIR / 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR / 'staticfiles/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
